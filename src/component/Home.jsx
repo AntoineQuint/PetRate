5f8b74f058de0503259f4aaab042d6bfa210ca98
hamid89 <hamid.khurshid24@gmail.com>
2025-02-20 15:52:00 +0100

    search Bar added

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,14 +1,13 @@
-import { Link } from "react-router-dom";
 import AddNewPet from "./AddNewPet.jsx";
 import ShowPets from "./ShowPet.jsx";
-
+import SearchBar from "./searchBar.jsx";
 
 export default function Home({ petArr, callbackRemovePet, setNewPet, callLikes }) {
   return (
     <div className="home">
       <h1></h1>
       
-
+      <SearchBar petsArr={petArr} />
 
       <ShowPets petArr={petArr} callbackRemovePet={callbackRemovePet} callLikes={callLikes} />
 
